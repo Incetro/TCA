f5563d0b1dfcc2f588060639bca472da3eb9cd91
Gleb Kovalenko <yourpendos@gmail.com>
2023-11-03 14:29:32 +0300

    feat: add AlertState init with title and message

--- Sources/ComposableArchitecture/BackwardCompatibility/AlertState+Init.swift
+++ Sources/ComposableArchitecture/BackwardCompatibility/AlertState+Init.swift
@@ -9,13 +9,25 @@ extension AlertState {
     
     public init(
         title: String,
+        message: String? = nil,
         primaryButton: ButtonState<Action>,
         secondaryButton: ButtonState<Action>
     ) {
         self.init(
             title: TextState(title),
+            message: message == nil ? nil : TextState(message.unsafelyUnwrapped),
             primaryButton: primaryButton,
             secondaryButton: secondaryButton
+        )
+    }
+    public init(
+        title: String,
+        message: String? = nil
+    ) {
+        self.init(
+            title: TextState(title),
+            message: message == nil ? nil : TextState(message.unsafelyUnwrapped),
+            dismissButton: nil
         )
     }
 }
