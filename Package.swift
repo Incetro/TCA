ff6aa21b78d71f703e49c217884ed3bf8d66ba8e
TraxxasWyls <savinov.dsta@gmail.com>
2024-07-23 15:39:52 +0300

    hotfix: child dependencies version issue

--- Package.swift
+++ Package.swift
@@ -29,8 +29,8 @@ let package = Package(
     .package(url: "https://github.com/pointfreeco/swift-identified-collections", from: "1.1.0"),
     .package(url: "https://github.com/pointfreeco/swift-macro-testing", from: "0.2.0"),
     .package(url: "https://github.com/pointfreeco/swift-perception", from: "1.1.7"),
-    .package(url: "https://github.com/pointfreeco/swiftui-navigation", from: "1.1.0"),
-    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.1.0"),
+    .package(url: "https://github.com/pointfreeco/swiftui-navigation", exact: "1.5.0"),
+    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", exact: "1.1.2"),
     .package(url: "https://github.com/swiftlang/swift-syntax", "509.0.0"..<"601.0.0-prerelease"),
   ],
   targets: [
